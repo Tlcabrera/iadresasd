56797800cf99383fa98d64ced5789bd46781abed
Tatiana Cabrera Vargas <84605458+Tlcabrera@users.noreply.github.com>
2023-08-22 16:39:02 -0500

    Base para cargar archivos

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import io
+import os
 
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile,File
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
@@ -15,40 +17,8 @@ from models.model import Prompt
 app=FastAPI()
 load_dotenv()
 
-import os
 api_key = os.environ["OPENAI_API_KEY"]
 
-# connect your Google Drive
-reader = PdfReader('./E54051222045753R001297402700.pdf')
-print(reader)
-
-# read data from the file and put them into a variable called raw_text
-raw_text = ''
-for i, page in enumerate(reader.pages):
-    text = page.extract_text()
-    if text:
-        raw_text += text
-
-print(raw_text[:100])
-# We need to split the text that we read into smaller chunks so that during information retreival we don't hit the token size limits. 
-
-text_splitter = CharacterTextSplitter(        
-    separator = "\n",
-    chunk_size = 1000,
-    chunk_overlap  = 200,
-    length_function = len,
-)
-texts = text_splitter.split_text(raw_text)
-
-print(len(texts))
-embeddings = OpenAIEmbeddings()
-docsearch = FAISS.from_texts(texts, embeddings)
-docsearch
-
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-
-chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
 """
 query = "cual es el nombre completo del paciente"
@@ -57,10 +27,64 @@ print(chain.run(input_documents=docs, question=query))
 """
 
 #endopoints
+async def uploadfile(file: UploadFile = File(...)):
+    pdf_buffer = io.BytesIO(await file.read())
+    reader = PdfReader(pdf_buffer)
+    file_path = os.path.join(os.environ["UPLOAD_DIR"], file.filename)
+    with open(file_path, "wb") as f:
+        f.write(pdf_buffer.getbuffer())
+        
+
+    return {"file_path": file_path}
+
 @app.post("/juridica")  
-def generate_response_pdf(prompt:Prompt):
-   query = prompt.text
-   docs = docsearch.similarity_search(query)
-   return chain.run(input_documents=docs, question=query)
+async def generate_response_pdf(prompt:Prompt, file: UploadFile = File(...)):
+   
+    #Captura el prompt
+    query = prompt.text
+    #Carga el archivo
+    pdf_buffer=uploadfile(file)
+    print(pdf_buffer)
+    
 
 
+
+    # read data from the file and put them into a variable called raw_text
+    raw_text = ''
+    for i, page in enumerate(reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
+
+    print(raw_text[:100])
+    # We need to split the text that we read into smaller chunks so that during information retreival we don't hit the token size limits. 
+
+    text_splitter = CharacterTextSplitter(        
+        separator = "\n",
+        chunk_size = 1000,
+        chunk_overlap  = 200,
+        length_function = len,
+    )
+    texts = text_splitter.split_text(raw_text)
+
+    print(len(texts))
+    embeddings = OpenAIEmbeddings()
+    docsearch = FAISS.from_texts(texts, embeddings)
+   
+
+    from langchain.chains.question_answering import load_qa_chain
+    from langchain.llms import OpenAI
+
+    chain = load_qa_chain(OpenAI(), chain_type="stuff")
+    print(reader)
+  
+
+    docs = docsearch.similarity_search(query)
+    return chain.run(input_documents=docs, question=query)
+
+
+"""
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+"""
